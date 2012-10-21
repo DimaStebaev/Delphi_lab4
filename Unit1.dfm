c3fb86c21da49e2487671a5dd168259c847bfcdf
Dima Stebaev <dimastebaev@gmail.com>
2012-10-21 21:42:26 +0300

    Реализовал все требования

--- Unit1.dfm
+++ Unit1.dfm
@@ -11,6 +11,7 @@ object Form1: TForm1
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object StringGrid: TStringGrid
@@ -20,8 +21,31 @@ object Form1: TForm1
     Height = 606
     Align = alClient
     Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goEditing]
+    PopupMenu = PopupMenu1
     TabOrder = 0
     OnDrawCell = StringGridDrawCell
-    OnSetEditText = StringGridSetEditText
+    OnKeyPress = StringGridKeyPress
+    OnMouseDown = StringGridMouseDown
+    OnSelectCell = StringGridSelectCell
+  end
+  object PopupMenu1: TPopupMenu
+    Left = 520
+    Top = 224
+    object N1: TMenuItem
+      Caption = #1059#1085#1080#1095#1090#1086#1078#1080#1090#1100' '#1074#1099#1076#1077#1083#1077#1085#1085#1091#1102' '#1089#1090#1088#1086#1082#1091
+      OnClick = N1Click
+    end
+    object N2: TMenuItem
+      Caption = #1042#1089#1090#1072#1074#1080#1090#1100' '#1087#1091#1089#1090#1091#1102' '#1089#1090#1088#1086#1082#1091' '#1087#1086#1089#1083#1077' '#1074#1099#1076#1077#1083#1077#1085#1085#1086#1081
+      OnClick = N2Click
+    end
+    object N3: TMenuItem
+      Caption = #1059#1085#1080#1095#1090#1086#1078#1080#1090#1100' '#1074#1099#1076#1077#1083#1077#1085#1085#1099#1081' '#1089#1090#1086#1083#1073#1080#1082
+      OnClick = N3Click
+    end
+    object N4: TMenuItem
+      Caption = #1042#1089#1090#1072#1074#1080#1090#1100' '#1087#1091#1089#1090#1086#1081' '#1089#1090#1086#1083#1073#1077#1094' '#1087#1086#1089#1083#1077' '#1074#1099#1076#1077#1083#1077#1085#1085#1086#1075#1086
+      OnClick = N4Click
+    end
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,17 +4,32 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Grids;
+  Dialogs, Grids, Menus;
 
 type
   TForm1 = class(TForm)
     StringGrid: TStringGrid;
+    PopupMenu1: TPopupMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
     procedure StringGridDrawCell(Sender: TObject; ACol, ARow: Integer;
       Rect: TRect; State: TGridDrawState);
-    procedure StringGridSetEditText(Sender: TObject; ACol, ARow: Integer;
-      const Value: String);
+    procedure StringGridKeyPress(Sender: TObject; var Key: Char);
+    procedure StringGridSelectCell(Sender: TObject; ACol, ARow: Integer;
+      var CanSelect: Boolean);
+    procedure Recalc();
+    procedure FormCreate(Sender: TObject);
+    procedure N1Click(Sender: TObject);
+    procedure StringGridMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure N3Click(Sender: TObject);
+    procedure N2Click(Sender: TObject);
+    procedure N4Click(Sender: TObject);
   private
-    { Private declarations }
+    mas: array of array of double;
+    lastRBC: TGridCoord;
   public
     { Public declarations }
   end;
@@ -25,6 +40,33 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Recalc;
+var
+table: Tstringgrid;
+i, j: integer;
+sum, x: double;
+begin
+table := stringgrid;
+for i:=table.FixedRows to table.RowCount-2 do
+begin
+    sum := 0;
+    for j:=table.FixedCols to table.RowCount-2 do
+      if tryStrToFloat(Table.Cells[j,i], x) then
+        sum:=sum+x
+      else Table.Cells[j,i] := '0';
+    table.Cells[table.ColCount-1, i] := FloatToStr(sum);
+end;
+for j:=Table.FixedCols to table.ColCount-1 do
+begin
+    sum:=0;
+    for i:=table.FixedRows to table.RowCount-2 do
+      if tryStrToFloat(Table.Cells[j,i], x) then
+        sum:=sum+x
+      else Table.Cells[j,i] := '0';
+    table.Cells[j, table.RowCount-1] := FloatToStr(sum);
+end;
+end;
 
 procedure TForm1.StringGridDrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState);
@@ -40,10 +82,106 @@ begin
 end;
 end;
 
-procedure TForm1.StringGridSetEditText(Sender: TObject; ACol,
-  ARow: Integer; const Value: String);
+procedure TForm1.StringGridKeyPress(Sender: TObject; var Key: Char);
+var
+r,c: integer;
 begin
-showmessage('event');
+r := stringgrid.Row;
+c := stringgrid.Col;
+if (r<0) or (c<0) then exit;
+if key = char(vk_return) then
+begin
+  Recalc;
+  exit;
+end;
+if (key in [',','.']) then key:=decimalseparator;
+if not (key in ['0'..'9', decimalseparator, '-', char(vk_Back)]) then key:=#0
+else if (key = DecimalSeparator) and (pos(decimalseparator, stringgrid.Cells[c,r])>0) then key:=#0
+else if (key = '-') and (pos('-', stringgrid.Cells[c,r])>0) then key:=#0;
+end;
+
+procedure TForm1.StringGridSelectCell(Sender: TObject; ACol, ARow: Integer;
+  var CanSelect: Boolean);
+begin
+Recalc;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+Recalc;
+end;
+
+procedure TForm1.N1Click(Sender: TObject);
+var
+i, j: integer;
+begin
+if (lastRBC.X >=0) and (lastRBC.Y>=1) and (lastRBC.Y<stringgrid.RowCount-1) then
+begin
+  for i:=lastRBC.Y to stringgrid.RowCount-2 do
+    for j:=0 to stringgrid.ColCount-1 do
+      stringgrid.Cells[j,i] := stringgrid.Cells[j,i+1];
+  stringgrid.RowCount:=stringgrid.RowCount-1;
+end;
+end;
+
+procedure TForm1.StringGridMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+if Button = mbRight then
+  lastRBC := stringgrid.MouseCoord(x,y);
+end;
+
+procedure TForm1.N3Click(Sender: TObject);
+var
+i, j: integer;
+begin
+if (lastRBC.X >=1) and (lastRBC.Y>=0) and (lastRBC.X<stringgrid.ColCount-1) then
+begin
+  for i:=0 to stringgrid.RowCount-1 do
+    for j:=lastRBC.X to stringgrid.ColCount-2 do
+      stringgrid.Cells[j,i] := stringgrid.Cells[j+1,i];
+  stringgrid.ColCount:=stringgrid.ColCount-1;
+end;
+end;
+
+procedure TForm1.N2Click(Sender: TObject);
+var
+i, j: integer;
+begin
+if stringgrid.RowCount = 11 then
+begin
+    messagedlg('Максимальное число строк - 11', mtInformation, [mbok], 0);
+    exit;
+end;
+if (lastRBC.X >=0) and (lastRBC.Y>=0)and (lastRBC.Y<stringgrid.RowCount-1) then
+begin
+  stringgrid.RowCount:=stringgrid.RowCount+1;
+  for i:=stringgrid.RowCount-2 downto lastRBC.Y+1 do
+    for j:=0 to stringgrid.ColCount-1 do
+      stringgrid.Cells[j,i+1] := stringgrid.Cells[j,i];
+  for j:=stringgrid.FixedCols to stringgrid.ColCount-1 do
+      stringgrid.Cells[j,lastRBC.Y+1] := '0';
+end;
+end;
+
+procedure TForm1.N4Click(Sender: TObject);
+var
+i, j: integer;
+begin
+if stringgrid.ColCount = 11 then
+begin
+    messagedlg('Максимальное число столбцов - 11', mtInformation, [mbok], 0);
+    exit;
+end;
+if (lastRBC.X >=0) and (lastRBC.Y>=0)and (lastRBC.X<stringgrid.ColCount-1) then
+begin
+  stringgrid.ColCount:=stringgrid.ColCount+1;
+  for i:=stringgrid.ColCount-2 downto lastRBC.X+1 do
+    for j:=0 to stringgrid.RowCount-1 do
+      stringgrid.Cells[i+1,j] := stringgrid.Cells[i,j];
+  for j:=stringgrid.FixedRows to stringgrid.RowCount-1 do
+      stringgrid.Cells[lastRBC.X+1,j] := '0';
+end;
 end;
 
 end.
